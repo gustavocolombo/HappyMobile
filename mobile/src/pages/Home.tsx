0f5c849f2d79f702678836fcf5d61f081910b548
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-10-28 21:05:42 -0300

    criando a página inicial

---
+++ mobile/src/pages/Home.tsx
@@ -0,0 +1,77 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions } from 'react-native';
+import Logo from '../images/logo.png';
+import { Feather } from '@expo/vector-icons';
+
+export default function Home(){
+  
+  const navigation = useNavigation();
+
+  function handleNavigateToOrphanageMap(){
+    navigation.navigate('OrphanagesMap');
+  }
+
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.home}>
+        <Image source={require('../images/logo.png')}/>
+        <Text style={styles.textHome}> happy </Text>
+
+        <Text style={styles.footerText}> Quixadá, Ceará </Text>
+        
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.navigateToMap} onPress={handleNavigateToOrphanageMap}>
+            <Feather name="arrow-right" size={20} color="#fff"/>
+          </TouchableOpacity>
+          
+       </View>
+      </View>
+    </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  home: {
+    backgroundColor: '#2AB5D1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  textHome: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 46,
+    color: '#fff',
+  }, 
+  
+  footer: {
+    position:'absolute',
+    right:24,
+    bottom: 32,
+
+    borderRadius: 20,
+    height: 46,
+    paddingRight: 24,
+  },
+
+  footerText:{
+    fontFamily:'Nunito_700Bold',
+    color: '#fff',
+  },
+
+  navigateToMap: {
+    width:56,
+    height:56,
+    backgroundColor: '#FFD666',
+    borderRadius:20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -7,11 +7,13 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OprhanageMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import Home from './pages/Home';
 
 export default function Routes(){
   return (
     <NavigationContainer>
       <Navigator screenOptions={{headerShown: false }}>
+        <Screen name="Home" component={ Home } /> 
         <Screen name="OrphanagesMap" component={ OprhanageMap }/>
         <Screen name="OrphanageDetails" component={ OrphanageDetails }/>
       </Navigator>
